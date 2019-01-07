1ff1cbd9a7da66b91d7323cb3e7e69cc9a49227e
Dani Lyn <dlyn1110@gmail.com>
2019-01-07 21:48:53 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,6 +3,8 @@ module ArtistsHelper
   def display_artist
     if song.artist
       link_to song.artist.name, artist_path(song.artist)
+    else
+      link_to 'Add Arist', edit song_path(song)
   end
 end
 
